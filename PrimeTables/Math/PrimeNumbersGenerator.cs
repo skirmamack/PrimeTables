2a0626db0af070441788666656eedbe2f4c92cda
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:39:19 +0000

    Prime numbers failing tests passed now

--- PrimeTables/Math/PrimeNumbersGenerator.cs
+++ PrimeTables/Math/PrimeNumbersGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace PrimeTables.Math
+﻿using System.Collections.Generic;
+
+namespace PrimeTables.Math
 {
     public class PrimeNumbersGenerator : IPrimeNumbersGenerator
     {
@@ -9,7 +11,32 @@
                 return new int[0];
             }
 
-            return new[] {1, 1};
+            var result = new List<int>();
+            var numberToCheck = 1;
+            while (primeNumbersCount > 0)
+            {
+                numberToCheck++;
+                if (IsPrimeNumber(numberToCheck))
+                {
+                    result.Add(numberToCheck);
+                    primeNumbersCount--;
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static bool IsPrimeNumber(int numberToCheck)
+        {
+            var number = 2;
+            while (number < numberToCheck)
+            {
+                var timesCount = numberToCheck / number;
+                if (timesCount * number == numberToCheck) return false;
+
+                number++;
+            }
+            return true;
         }
     }
 }
--- PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
+++ PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
@@ -23,9 +23,9 @@ namespace PrimeTables.Tests
             Assert.That(result.Length, Is.EqualTo(0));
         }
 
-        [TestCase(1, new long[] { 2 })]
-        [TestCase(2, new long[] { 2, 3 })]
-        [TestCase(3, new long[] { 2, 3, 5 })]
+        [TestCase(1, new[] { 2 })]
+        [TestCase(2, new[] { 2, 3 })]
+        [TestCase(3, new[] { 2, 3, 5 })]
         public void should_generate_correct_prime_numbers(int primeNumbersCount, int[] expectedResult)
         {
             var result = _generator.Generate(primeNumbersCount);
