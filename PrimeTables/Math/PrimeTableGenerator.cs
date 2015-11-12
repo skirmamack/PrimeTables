13d04bd68a6795484bc26133bcdb4aaeca59e70d
skirmamack <skirmamack@yahoo.com>
2015-11-12 07:14:19 +0000

    First prime table row failing test passed

--- PrimeTables/Math/PrimeTableGenerator.cs
+++ PrimeTables/Math/PrimeTableGenerator.cs
@@ -2,9 +2,28 @@
 {
     public class PrimeTableGenerator : IPrimeTableGenerator
     {
+        private readonly IPrimeNumbersGenerator _primeNumbersGenerator;
+
+        public PrimeTableGenerator()
+            : this(new PrimeNumbersGenerator())
+        {
+        }
+
+        public PrimeTableGenerator(IPrimeNumbersGenerator primeNumbersGenerator)
+        {
+            _primeNumbersGenerator = primeNumbersGenerator;
+        }
+
         public int[,] Generate(int primeNumbersCount)
         {
             var result = new int[primeNumbersCount + 1, primeNumbersCount + 1];
+            var primeNumbers = _primeNumbersGenerator.Generate(primeNumbersCount);
+
+            for(var primeIndex = 0; primeIndex < primeNumbers.Length; primeIndex++)
+            {
+                result[0, primeIndex + 1] = primeNumbers[primeIndex];
+            }
+
             return result;
         }
     }
