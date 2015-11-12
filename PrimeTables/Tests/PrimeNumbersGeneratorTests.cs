dcc73916c5891fbef8c145ce3f1ab1e952637fe9
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:30:16 +0000

    Failing tests for positive primes count added

--- PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
+++ PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
@@ -22,5 +22,15 @@ namespace PrimeTables.Tests
 
             Assert.That(result.Length, Is.EqualTo(0));
         }
+
+        [TestCase(1, new long[] { 2 })]
+        [TestCase(2, new long[] { 2, 3 })]
+        [TestCase(3, new long[] { 2, 3, 5 })]
+        public void should_generate_correct_prime_numbers(int primeNumbersCount, int[] expectedResult)
+        {
+            var result = _generator.Generate(primeNumbersCount);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
