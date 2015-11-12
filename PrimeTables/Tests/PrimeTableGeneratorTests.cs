cd9b274c7b5dd5fe8581f7ccec402730638117ef
skirmamack <skirmamack@yahoo.com>
2015-11-12 07:00:22 +0000

    failing test, checking prime table size, added

--- PrimeTables/Tests/PrimeTableGeneratorTests.cs
+++ PrimeTables/Tests/PrimeTableGeneratorTests.cs
@@ -14,5 +14,14 @@ namespace PrimeTables.Tests
         {
             _primeTableGenerator = new PrimeTableGenerator();
         }
+
+        [Test]
+        public void should_generate_table_of_correct_size()
+        {
+            var primeTable = _primeTableGenerator.Generate(_testPrimesCount);
+
+            Assert.That(primeTable.GetUpperBound(0), Is.EqualTo(_testPrimesCount));
+            Assert.That(primeTable.GetUpperBound(1), Is.EqualTo(_testPrimesCount));
+        }
     }
 }
