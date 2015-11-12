272e5468b49e74952e80f8b30e2509809ca9f3ee
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:25:38 +0000

    Non positive primes count result implemented

--- PrimeTables/Math/PrimeNumbersGenerator.cs
+++ PrimeTables/Math/PrimeNumbersGenerator.cs
@@ -4,6 +4,11 @@
     {
         public int[] Generate(int primeNumbersCount)
         {
+            if (primeNumbersCount <= 0)
+            {
+                return new int[0];
+            }
+
             return new[] {1, 1};
         }
     }
