26b33e0229a2556b9a6d139715acf980cc0b6244
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:13:58 +0000

    Empty prime numbers generator test class created

---
+++ PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using PrimeTables.Math;
+
+namespace PrimeTables.Tests
+{
+    [TestFixture]
+    public class PrimeNumbersGeneratorTests
+    {
+        private PrimeNumbersGenerator _generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _generator = new PrimeNumbersGenerator();
+        }
+    }
+}
