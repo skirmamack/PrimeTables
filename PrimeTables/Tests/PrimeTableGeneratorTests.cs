a1a18083ac6d91fe59ffd973bf1cc40ffef3a058
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:56:54 +0000

    Prime table generator test class added

---
+++ PrimeTables/Tests/PrimeTableGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PrimeTables.Math;
+
+namespace PrimeTables.Tests
+{
+    [TestFixture]
+    public class PrimeTableGeneratorTests
+    {
+        private PrimeTableGenerator _primeTableGenerator;
+        private int _testPrimesCount = 15;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _primeTableGenerator = new PrimeTableGenerator();
+        }
+    }
+}
