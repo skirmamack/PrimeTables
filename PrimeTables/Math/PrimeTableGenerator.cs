c3213a6e49165e6045deb45210586d6cb4c2d9c6
skirmamack <skirmamack@yahoo.com>
2015-11-12 07:45:24 +0000

    Prime table generator refactored

--- PrimeTables/Math/PrimeTableGenerator.cs
+++ PrimeTables/Math/PrimeTableGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace PrimeTables.Math
+﻿using System.Collections.Generic;
+
+namespace PrimeTables.Math
 {
     public class PrimeTableGenerator : IPrimeTableGenerator
     {
@@ -19,18 +21,29 @@
             var result = new int[primeNumbersCount + 1, primeNumbersCount + 1];
             var primeNumbers = _primeNumbersGenerator.Generate(primeNumbersCount);
 
-            for(var primeIndex = 0; primeIndex < primeNumbers.Length; primeIndex++)
-            {
-                result[0, primeIndex + 1] = primeNumbers[primeIndex];
-                result[primeIndex + 1, 0] = primeNumbers[primeIndex];
-
-                for (var rowIndex = 1; rowIndex <= primeNumbers.Length; rowIndex++)
-                {
-                    result[rowIndex, primeIndex + 1] = primeNumbers[rowIndex - 1]*primeNumbers[primeIndex];
-                }
-            }
+            FillCells(result, primeNumbers);
 
             return result;
+        }
+
+        private static void FillCells(int[,] result, IReadOnlyList<int> primeNumbers)
+        {
+            for (var rowIndex = 1; rowIndex <= primeNumbers.Count; rowIndex++)
+            {
+                var primeNumber = primeNumbers[rowIndex - 1];
+                result[0, rowIndex] = primeNumber;
+                result[rowIndex, 0] = primeNumber;
+
+                FillRowInnerCells(result, rowIndex, primeNumbers);
+            }
+        }
+
+        private static void FillRowInnerCells(int[,] result, int rowIndex, IReadOnlyList<int> primeNumbers)
+        {
+            for (var columnIndex = 1; columnIndex <= primeNumbers.Count; columnIndex++)
+            {
+                result[columnIndex, rowIndex] = primeNumbers[columnIndex - 1] * primeNumbers[rowIndex - 1];
+            }
         }
     }
 }
