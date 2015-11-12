7df4fffeab1041608ac56fa307abba1bd3baae33
skirmamack <skirmamack@yahoo.com>
2015-11-12 06:46:51 +0000

    Test case added for generating prime numbers in range 1-100 (from google, to check if works with larger set)

--- PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
+++ PrimeTables/Tests/PrimeNumbersGeneratorTests.cs
@@ -26,6 +26,7 @@ namespace PrimeTables.Tests
         [TestCase(1, new[] { 2 })]
         [TestCase(2, new[] { 2, 3 })]
         [TestCase(3, new[] { 2, 3, 5 })]
+        [TestCase(25, new[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 })]
         public void should_generate_correct_prime_numbers(int primeNumbersCount, int[] expectedResult)
         {
             var result = _generator.Generate(primeNumbersCount);
